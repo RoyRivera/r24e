111457bf08f830e6ce4116e27fde97f5735be3bd
Roy Rivera <rrivera@growthaccelerationpartners.com>
2015-04-28 15:34:59 -0500

    Users creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+4.times do |index|
+  User.create!({
+    username: "user#{index}",
+    email: "user#{index}@domain.com",
+    picture: "image#{index}.jpg",
+    website: 'http://www.wearegap.com',
+    password: 'testpass23',
+    password_confirmation: 'testpass23'
+    })
+end
+
+# Users without website
+4.times do |index|
+  User.create!({
+    username: "no_website_user#{index}",
+    email: "user_without_website#{index}@domain.com",
+    picture: "image#{index}.jpg",
+    website: "invalid website#{index} dot com",
+    password: 'testpass23',
+    password_confirmation: 'testpass23'
+    })
+end
+
+# Users without picture
+4.times do |index|
+  User.create!({
+    username: "no_picture_user#{index}",
+    email: "user_without_picture#{index}@domain.com",
+    picture: '',
+    website: 'http://www.wearegap.com',
+    password: 'testpass23',
+    password_confirmation: 'testpass23'
+    })
+end
+
