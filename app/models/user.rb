90c46bf6d73dd7733fbb47748f5989dd875cda43
Roy Rivera <rrivera@growthaccelerationpartners.com>
2015-04-28 14:32:22 -0500

    Additional method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
     config.login_field = :email
     config.merge_validates_length_of_password_field_options({:minimum => 6, :maximum => 20})
   end
+
+  def valid_website?(url = website)
+    url = URI.parse(url) rescue false
+    url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
+  end
+
 end
